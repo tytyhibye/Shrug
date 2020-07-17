8e2db7ff4935fcf13bf7ec4788db6ea8116d9395
Tyler Bates <Fondlytyler@gmail.com>
2020-07-17 11:35:29 -0700

    create app for yelp fusion api and generate key/id.

---
+++ src/components/YelpApi.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+async getRestaurants(restaurantPlaceHolder) {
+  try {
+    let request = await fetch(`https://api.yelp.com/v3/${restaurantPlaceHolder}`);
+    if (request.ok && request.status === 200) {
+      let restaurantObject = await request.json();
+      // call function for displaying restaurant data here.
+    } else {
+      request = false;
+    }
+  } catch (error) {
+    return false;
+  }
+}
+
+export default getRestaurants
